46fa8a94e75fa3359cb7d11a0ed5e03aa9c27aa8
Mathew Dwyer <120291154+Dwyer9@users.noreply.github.com>
2023-02-23 07:44:58 +1100

    5 Round game

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let computerChoice;
 let playerChoice;
+let lastWinner;
 
 const getComputerChoice = function () {
   const random = Math.floor(Math.random() * 3) + 1;
@@ -10,23 +11,48 @@ const getComputerChoice = function () {
   } else {
     computerChoice = 'scissors';
   }
-  console.log(computerChoice);
+  // console.log(computerChoice);
 };
 
 const playRound = function (playerSelection) {
   getComputerChoice();
   playerChoice = playerSelection.toLowerCase();
   if (computerChoice === playerChoice) {
-    console.log('No result');
+    // console.log('No result');
   } else if (
     (playerChoice === 'rock' && computerChoice === 'scissors') ||
     (playerChoice === 'paper' && computerChoice === 'rock') ||
     (playerChoice === 'scissors' && computerChoice === 'paper')
   ) {
+    // console.log('Player Wins!');
+    lastWinner = 1;
+  } else {
+    // console.log('Computer Wins!');
+    lastWinner = 2;
+  }
+};
+
+// playRound('rock');
+
+const game = function () {
+  playerScore = 0;
+  computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    playerSelection = prompt('Enter Selection').toLowerCase();
+    playRound(playerSelection);
+    if (lastWinner === 1) {
+      playerScore++;
+    } else if (lastWinner === 2) {
+      computerScore++;
+    }
+  }
+
+  if (playerScore > computerScore) {
     console.log('Player Wins!');
   } else {
     console.log('Computer Wins!');
   }
 };
 
-playRound('rock');
+game();
